f027448d7f86c999a2a3443cd7fb8391db85ab2d
yusk <157600722+yusk2@users.noreply.github.com>
2024-01-24 14:28:28 +0200

    My first ever program made.

---
+++ Calculator.py
@@ -0,0 +1,52 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+print('Choose operation:')
+print('1. Add')
+print('2. Subtract')
+print('3. Multiply')
+print('4. Divide')
+
+while True:
+    choice = input('Select your operation (1/2/3/4): ')
+# Basically, if the user didn't input 1, 2, 3 or 4, the program would stop running.
+    if choice not in ('1', '2', '3', '4'):
+        print('Invalid choice. Exiting.')
+        break
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+# If the user did not input an valid number, the program would tell you to input a valid number.
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+
+    if choice == '1':
+        print(num1, '+', num2, '=', add(num1, num2))
+    elif choice == '2':
+        print(num1, '-', num2, '=', subtract(num1, num2))
+    elif choice == '3':
+        print(num1, '*', num2, '=', multiply(num1, num2))
+    elif choice == '4':
+        if num2 == 0:
+            print("Cannot divide by zero.") #if the second number was 0, the program would tell you it can't divide by 0.
+        else:
+            print(num1, '/', num2, '=', divide(num1, num2))
+
+    next_calculation = input('Do you want to perform another calculation? (yes/no): ')
+    if next_calculation.lower() != 'yes': #if you chose anything different from "yes", the program would stop working.
+        break
