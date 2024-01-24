4583c5f187c9a1875a6038271f3e698c8855c363
yusk <157600722+yusk2@users.noreply.github.com>
2024-01-24 14:43:44 +0200

    Updated Calculator.py
    Updated the code so there would be no problems.

--- Calculator.py
+++ Calculator.py
@@ -24,16 +24,16 @@ while True:
     choice = input('Select your operation (1/2/3/4): ')
 
     if choice not in ('1', '2', '3', '4'):
-        print('Invalid choice. Exiting.') # Basically, if the user didn't input 1, 2, 3 or 4, the program would stop running.
-        break
+        print('Invalid choice. Exiting.')  # Basically, if the user didn't input 1, 2, 3 or 4, the program would stop
+        break                              # running.
 
     try:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
     except ValueError:
-        print("Invalid input. Please enter a number.") # If the user did not input an valid number, the program would tell you to input a valid number.
-        continue
+        print("Invalid input. Please enter a number.")  # If the user did not input a valid number, the program would
+        continue                                        # tell you to input a valid number.
 
     if choice == '1':
         print(num1, '+', num2, '=', add(num1, num2))
@@ -43,10 +43,10 @@ while True:
         print(num1, '*', num2, '=', multiply(num1, num2))
     elif choice == '4':
         if num2 == 0:
-            print("Cannot divide by zero.") #if the second number was 0, the program would tell you it can't divide by 0.
-        else:
+            print("Cannot divide by zero.")  # if the second number was 0, the program would tell you it can't divide
+        else:                                # by 0.
             print(num1, '/', num2, '=', divide(num1, num2))
 
     next_calculation = input('Do you want to perform another calculation? (yes/no): ')
-    if next_calculation.lower() != 'yes': #if you chose anything different from "yes", the program would stop working.
+    if next_calculation.lower() != 'yes':  # if you chose anything different from "yes", the program would stop working.
         break
