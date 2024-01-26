2bed1eef6cb580c70b7318212664ea0e57bde9c9
yusk <157600722+yusk2@users.noreply.github.com>
2024-01-26 20:02:35 +0200

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -4,14 +4,14 @@ name = input('What is your name? ')
 print('Hello', name, 'try and guess my number(within 1-20): ')
 number = random.randint(1, 20)
 guess = int(input())
-for guessesTaken in range(1, 7):
+for guessesTaken in range(1, 7):  # You got a max of 7 tries.
     print('Choose your number: ')
     if guess > number:
         print('Your number is too high, try again!')
     elif guess < number:
         print('Your number is too low, try again!')
     else:
-        break
+        break  # If the number is correct, the loop will break.
 
 if guess == number:
     print('You guessed my number,', name)
